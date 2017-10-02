36bdfcc15a5f8a2c357c5ce65cdf8b91fc3eb830
Ladydiana <diana.culincu@gmail.com>
2017-10-02 20:50:34 +0300

    Added enemy movement

--- Assets/Chibi Mummy/Misc/Scripts/CharMovementSlow.cs
+++ Assets/Chibi Mummy/Misc/Scripts/CharMovementSlow.cs
@@ -8,11 +8,20 @@ public class CharMovementSlow : MonoBehaviour
 	public bool grounded = false;
 	public bool doubleJump = false;
 	public Transform groundCheck;
-	public float groundRadius = 0.2f;
+	public float groundRadius = 0.001f;
 	public LayerMask whatIsGround;
 	private Animator anim;
 	public Rigidbody rb;
 	public float vSpeed;
+
+
+	private Vector3 originalPosition;
+	private Quaternion originalRotation;
+	public float movementSpeed = 3.0f;
+	public float turnAroundX;
+	public float turnAroundStartX;
+	public float turnAroundZ;
+
 
 	void Awake()
 	{
@@ -32,20 +41,36 @@ public class CharMovementSlow : MonoBehaviour
 	}
 	void Update () 
 	{
-		/*if (Input.GetKeyDown("space") && anim.GetBool("isIdle"))
+		if (turnAroundX != 0 
+			&& Mathf.Round(transform.position.x) != turnAroundX 
+			&& Mathf.Round(turnAroundStartX) != Mathf.Round(transform.position.x) 
+			)
+
 		{
-			Jump();
-		}*/
+			transform.position += transform.forward * Time.deltaTime * movementSpeed;
+			Debug.Log ("Moving forward.");
+		} 
+		else if (turnAroundX != 0 
+				&& Mathf.Round(transform.position.x) == turnAroundX ) 
+		{
+			
+			transform.Rotate (0, 90, 0,  Space.Self);
+			transform.position += transform.forward * Time.deltaTime * movementSpeed;
+			Debug.Log ("Rotating other side");
+		}
+
+		else if (turnAroundX != 0 
+				&& Mathf.Round(turnAroundStartX) == Mathf.Round(transform.position.x))
+		{
+			transform.Rotate (0, -90, 0,  Space.Self);
+			transform.position += transform.forward * Time.deltaTime * movementSpeed;
+			Debug.Log("Rotating start");
+		}
+			
 	}
 
-	/*public void Jump ()
-	{
-		if (grounded && rb.velocity.y == 0)
-		{
-			anim.SetTrigger("isJump");
-            rb.AddForce(0,jumpSpeed,0, ForceMode.Impulse);
-		}
-	}*/
+
+		
 
 	void OnDrawGizmos ()
 	{
